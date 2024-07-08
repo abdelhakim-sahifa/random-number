b51353103c97d73eb3a7432db0f810d2b63aec12
hide-place <hideplace865@gmail.com>
2024-07-08 14:30:12 +0000

    Added Fun Fact Feature

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-
+//DOM elements :
 const label = document.getElementById('lbl');
 const modeToggle = document.getElementById('mode-toggle');
 const historyDiv = document.getElementById('history') ;
+const funFactLbl = document.getElementById('funFactLbl');
 
+//init
 
+var isdark = localStorage.getItem('isDark');
+if(isdark){
+    document.body.classList.add('dark-body');
+    modeToggle.classList.add('dark-mode-toggle');
+    modeToggle.classList.add('fa-sun');
+    label.style.border = 'solid 1px white';
+}
 
 
 var listOfHistory = JSON.parse(localStorage.getItem('historyList')) || [];
@@ -13,23 +22,51 @@ refreshHistory(listOfHistory);
 
 
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 
+
+//Event Listeners :
+label.addEventListener('mouseenter' , generateRandomNumber)
+
+modeToggle.addEventListener('click' , () => {
+    var backgroundColor = window.getComputedStyle(document.body).backgroundColor;
+    if(backgroundColor== 'rgb(255, 255, 255)'){
+        document.body.classList.add('dark-body');
+        modeToggle.classList.add('dark-mode-toggle');
+        modeToggle.classList.add('fa-sun');
+        label.style.border = 'solid 1px white';
+        historyDiv.style.border = 'solid 1px white';
+        localStorage.setItem('isDark', true);
+    }
+    else{
+        document.body.classList.remove('dark-body');
+        modeToggle.classList.remove('dark-mode-toggle');
+        modeToggle.classList.remove('fa-sun')
+        modeToggle.classList.add('fa-moon')
+        label.style.border = 'solid 1px black';
+        historyDiv.style.border = 'solid 1px black';
+        localStorage.removeItem('isDark');
+    }
+})
+
+
+
+//function :
 async function generateRandomNumber() {
     label.style.fontSize = '100px';
-   
-
+    funFactLbl.style.display = 'none'
     for(i=0 ; i < 10 ; i++){
-        var randomNumber = Math.floor(1000 + Math.random() * 9000);
+      var randomNumber = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+       
+      
         label.classList.add('red');
         label.innerText = randomNumber;
         await sleep(50);
     }
     label.classList.remove('red')
     label.classList.add('green');
+    randomNumber = pickRandomNumber()
     label.innerText = randomNumber;
+    await getFunFact(randomNumber)
     saveToHistory(randomNumber);
     copyToClipboard(randomNumber);
     await sleep(1000);
@@ -37,13 +74,98 @@ async function generateRandomNumber() {
     label.classList.remove('green')
    
 }
+function pickRandomNumber(){
+    var listOfRandomNumbers = []
 
-label.addEventListener('mouseenter' , generateRandomNumber)
+    for(var i = 10 ; i <= 10000 ; i*=10){
+        var randomNumber = Math.floor(Math.random() * i).toString().padStart(4, '0');
+        listOfRandomNumbers.push(randomNumber)
+         
+     }
+     
+
+     let randomIndex = Math.floor(Math.random() * 4);
+
+    
+     return  listOfRandomNumbers[randomIndex];
+     
+
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 
 
+function saveToHistory(theRandomNumber) {
+    if (listOfHistory.length == 3) {
+        listOfHistory.splice(0, 1);
+    }
+    listOfHistory.push(theRandomNumber);
+    localStorage.setItem('historyList', JSON.stringify(listOfHistory));
+    refreshHistory(listOfHistory);
+}
 
-// Function to copy text to clipboard
+function refreshHistory(listOfHistory){
+    historyDiv.innerHTML = '';
+    
+
+    if(listOfHistory != null){
+        for(var item of listOfHistory){
+            var  numberLabel = document.createElement('label');
+            numberLabel.innerHTML = `
+            
+            <label id="lbl${listOfHistory.indexOf(item)}" class="history-lbl" >${item}</label>
+        
+        
+            `;
+            historyDiv.appendChild(numberLabel);
+            
+            
+        }
+        
+
+        
+    }
+
+    else{
+        historyDiv.innerHTML = `
+            
+        <label>----</label>
+
+
+        `;
+    }
+
+
+}
+
+async function getFunFact(number){
+
+    var url = `http://numbersapi.com/${number}`;
+       fetch(url)
+           .then(response => {
+               if (!response.ok) {
+                   throw new Error('Network response was not ok ' + response.statusText);
+               }
+               return response.text();
+           })
+           .then(data => {
+               
+               funFactLbl.style.display = 'flex'
+               funFactLbl.textContent = data;
+               var theFunFcat = data;
+                
+               
+               
+           })
+           .catch(error => {
+               console.error('There has been a problem with your fetch operation:', error);
+               funFactLbl.textContent = 'Error fetching the text.';
+           });
+   }  
+
 function copyToClipboard(text) {
     function attemptCopy() {
         navigator.clipboard.writeText(text).then(function() {
@@ -70,84 +192,8 @@ function copyToClipboard(text) {
 
 
 
-var isdark = localStorage.getItem('isDark')
-
-if(isdark){
-    document.body.classList.add('dark-body');
-    modeToggle.classList.add('dark-mode-toggle');
-    modeToggle.classList.add('fa-sun');
-    label.style.border = 'solid 1px white';
-}
 
 
 
 
-
-modeToggle.addEventListener('click' , () => {
-    var backgroundColor = window.getComputedStyle(document.body).backgroundColor;
-    if(backgroundColor== 'rgb(255, 255, 255)'){
-        document.body.classList.add('dark-body');
-        modeToggle.classList.add('dark-mode-toggle');
-        modeToggle.classList.add('fa-sun');
-        label.style.border = 'solid 1px white';
-        historyDiv.style.border = 'solid 1px white';
-        localStorage.setItem('isDark', true);
-    }
-    else{
-        document.body.classList.remove('dark-body');
-        modeToggle.classList.remove('dark-mode-toggle');
-        modeToggle.classList.remove('fa-sun')
-        modeToggle.classList.add('fa-moon')
-        label.style.border = 'solid 1px black';
-        historyDiv.style.border = 'solid 1px black';
-        localStorage.removeItem('isDark');
-    }
-})
-
-
-
-function saveToHistory(theRandomNumber) {
-    if (listOfHistory.length == 3) {
-        listOfHistory.splice(0, 1);
-    }
-    listOfHistory.push(theRandomNumber);
-    localStorage.setItem('historyList', JSON.stringify(listOfHistory));
-    refreshHistory(listOfHistory);
-}
-
-
-
-function refreshHistory(listOfHistory){
-    historyDiv.innerHTML = '';
-    
-
-    if(listOfHistory != null){
-        for(var item of listOfHistory){
-            var  numberLabel = document.createElement('label');
-            numberLabel.innerHTML = `
-            
-            <label id="lbl${listOfHistory.indexOf(item)}" class="history-lbl" >${item}</label>
-        
-        
-            `;
-            historyDiv.appendChild(numberLabel);
-            console.log(item);
-            
-        }
-        console.log("------------");
-
-        
-    }
-
-    else{
-        historyDiv.innerHTML = `
-            
-        <label>----</label>
-
-
-        `;
-    }
-
-
-}
 
